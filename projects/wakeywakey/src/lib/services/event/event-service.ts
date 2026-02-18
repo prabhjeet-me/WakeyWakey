2cbbf9ec91d0feb64f5402caf817a145408086ea
Prabhjeet Singh <dev@prabhjeet.me>
2026-02-18 17:19:09 +0000

    feat(event): event service for firing events

---
+++ projects/wakeywakey/src/lib/services/event/event-service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { SpeechEvent, WakeWordEvent } from 'wakeywakey';
+
+@Injectable()
+export class EventService {
+  /**
+   * Fires when library loaded
+   */
+  readonly ready = new Subject<void>();
+
+  /**
+   * Fires when there is a message to log
+   */
+  readonly log = new Subject<string>();
+
+  /**
+   * Fires when there is an error
+   */
+  readonly exception = new Subject<Error>();
+
+  /**
+   * Fires when speech is detected
+   */
+  readonly speech = new Subject<SpeechEvent>();
+
+  /**
+   * Fires when wake word is detected
+   */
+  readonly wakeword = new Subject<WakeWordEvent>();
+
+  /**
+   * Fires when recording starts (after wake word detection)
+   */
+  readonly recording = new Subject<void>();
+
+  /**
+   * Fires silence is detected
+   */
+  readonly silence = new Subject<SpeechEvent>();
+}
