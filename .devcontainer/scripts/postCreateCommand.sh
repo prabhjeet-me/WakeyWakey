48e3ba50bf25d66fb6cb29cbcf99d870a29e70d4
Prabhjeet Singh <dev@prabhjeet.me>
2026-02-19 16:00:28 +0000

    chore: use ubuntu base image & python removed

--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -3,20 +3,9 @@ set -euo pipefail
 
 source .devcontainer/scripts/util.sh
 
-log blue "Updating owner for packages volume..."
+log blue "Updating npm"
 
-# Fix permissions
-sudo chown -R $USER:$USER /home/${USER}/.local
-
-pip install --upgrade pip
-
-log blue "Installing python packages..."
-
-# Install required packages
-pip install -r .devcontainer/others/requirements.txt
-
-# Add python alias
-echo "alias py='python'" >> ~/.bashrc
+npm install -g npm@11.10.0
 
 log blue "Installing node packages..."
 
