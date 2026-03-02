60cb08f2d7efa4561c6be7a3c57d7054517384e8
Prabhjeet Singh <dev@prabhjeet.me>
2026-03-02 07:10:14 +0000

    fix(orb): export AgentState

--- projects/wakeywakey/src/public-api.ts
+++ projects/wakeywakey/src/public-api.ts
@@ -3,6 +3,8 @@
 /*
  * Public API Surface of wakeywakey
  */
+export type { AgentState as WakeyWakeyAgentState } from './lib/components/orb-component/orb-component.type';
+
 export * from './lib/services';
 export * from './lib/wakeywakey-component';
 export * from './provider';
